fd479da1f0bac59a3666c93d0f7ed2175def60bb
Conan <conan.cook@gmail.com>
2016-03-04 11:45:28 +0000

    bump version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject banana "0.1.0"
+(defproject banana "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
