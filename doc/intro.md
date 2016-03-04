14bff182764726c97fe3c1577178207b3f4a2ed3
Conan <conan.cook@gmail.com>
2016-03-04 11:43:35 +0000

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to banana
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/banana/core_test.clj
@@ -0,0 +1,7 @@
+(ns banana.core-test
+  (:require [clojure.test :refer :all]
+            [banana.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
