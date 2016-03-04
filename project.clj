648045db3a84f2b64602dca53f73c59755729771
Conan <conan.cook@gmail.com>
2016-03-04 11:47:00 +0000

    second commit

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject banana "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
+  :scm {:name "git" :url "https://github.com/conan/banana"}
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
